c1e847ca9561f6fdce82444171dc3631d9d0043e
LATEEF-hub <latifkk@krok.edu.ua>
2023-09-01 07:45:48 +0100

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,32 @@ document.addEventListener("DOMContentLoaded", function()  {
                 alert("You clicked Submit!")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}!`);
+                runGame(gameType);
             }
         })
     }
+
+ runGame("addition");
+
 })
 /**
  * The main game "Loop" called when the Script is first loaded 
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
     //Create two random num btw 1 and 25
     let num1 = Math.floor(Math.random()*25)+1
     let num2 = Math.floor(Math.random()*25)+1
+    //create a variable to store the result of the operation
+    //    console.log(`num1: ${num1} | num2:${num2}`);
+    //Check which type of question we are dealing with, then call the correct method
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2); 
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -36,7 +48,10 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 function displaySubtractionQuestion() {
